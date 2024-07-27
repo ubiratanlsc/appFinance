71267dd17f0efd1d710bd5b7fabc3f1f0cfb0f27
ubiratanlsc <ubiratanlsc@gmail.com>
2024-07-27 00:33:11 -0300

    table

--- src/app/components/Table.jsx
+++ src/app/components/Table.jsx
@@ -1,57 +1,72 @@
+import React, { useEffect, useState } from "react";
+import Table from "@mui/joy/Table";
 
-import React from 'react';
-import Table from '@mui/joy/Table';
+const TableView = (props) => {
+  const [dados, setDados] = useState();
+  const classes = "" ? props.classes : "";
+  console.log(props.classes);
+  const porcentagens = [];
+  const categorias = [];
+  const valores = [];
+  useEffect(() => {
+    fetch("http://localhost:5000/transacao", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      cors: "cors",
+    })
+      .then((response) => response.json())
+      .then((data) => setDados(data))
+      .catch((error) => console.log(error));
+  }, []);
+  function createData(local, value, data, hora, tipo, subtipo) {
+    const valor = new Intl.NumberFormat("pt-BR", {
+      style: "currency",
+      currency: "BRL",
+    }).format(value);
 
-const TableView = () => {
+    // console.log(formattedValor);
+    return { local, valor, data, hora, tipo, subtipo };
+  }
+  const rows = [
+  ];
+  dados?.forEach(element => {
+    rows.push(createData(element.local, element.valor, element.data, element.hora, element.categoria, element.subcategoria))
+  });
 
-    function createData(name, calories, fat, carbs, protein) {
-        return { name, calories, fat, carbs, protein };
-    }
 
-    const rows = [
-        createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-        createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-        createData('Eclair', 262, 16.0, 24, 6.0),
-        createData('Cupcake', 305, 3.7, 67, 4.3),
-        createData('Gingerbread', 356, 16.0, 49, 3.9),
-    ];
-    return (
-        <div className=''>
-            <Table borderAxis={'yBetween'}
-                color="neutral"
-                size="sm"
-                stickyHeader
-                variant="outlined"
-                sx={{ width: '30%' }}
 
-            >
-                <thead>
-                    <tr>
-                        <th style={{ width: '40%' }}>Local</th>
-                        {/* <th>Nome</th> */}
-                        <th>Valor</th>
-                        <th>Data&nbsp;</th>
-                        <th>Hora&nbsp;</th>
-                        <th>Tipo&nbsp;</th>
-                        <th>Subtipo&nbsp;</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {rows.map((row) => (
-                        <tr key={row.name}>
-                            <th scope="row">{row.name}</th>
-                            {/* <td>{row.name}</td> */}
-                            <td>{row.calories}</td>
-                            <td>{row.fat}</td>
-                            <td>{row.carbs}</td>
-                            <td>{row.protein}</td>
-                            <td>{row.protein}</td>
-                        </tr>
-                    ))}
-                </tbody>
-            </Table>
-        </div>
-    );
-}
+
+  return (
+    <div className={`${props.classes}`}>
+      <table className="min-w-full">
+        <thead >
+          <tr>
+            <th className="text-center w-72 text-slate-100 bg-black border-collapse border-e-[1px] border-slate-600 rounded-tl-lg">Local</th>
+            <th className="text-center w-32 text-slate-100 bg-black border-collapse border-e-[1px] border-slate-600">Valor</th>
+            <th className="text-center w-32 text-slate-100 bg-black border-collapse border-e-[1px] border-slate-600">Data&nbsp;</th>
+            <th className="text-center w-32 text-slate-100 bg-black border-collapse border-e-[1px] border-slate-600">Hora&nbsp;</th>
+            <th className="text-center w-32 text-slate-100 bg-black border-collapse border-e-[1px] border-slate-600">Tipo&nbsp;</th>
+            <th className="text-center w-32 text-slate-100 bg-black rounded-tr-lg">Subtipo&nbsp;</th>
+          </tr>
+        </thead>
+        <tbody>
+          {rows.map((row, index) => (
+
+            <tr key={index}>
+              <th className="text-center text-slate-100 border-collapse border-e-[1px] border-slate-600">{row.local}</th>
+              <td className="text-center text-slate-100 border-collapse border-e-[1px] border-slate-600">{row.valor}</td>
+              <td className="text-center text-slate-100 border-collapse border-e-[1px] border-slate-600">{row.data}</td>
+              <td className="text-center text-slate-100 border-collapse border-e-[1px] border-slate-600">{row.hora}</td>
+              <td className="text-center text-slate-100 border-collapse border-e-[1px] border-slate-600">{row.tipo}</td>
+              <td className="text-center text-slate-100 border-collapse border-e-[1px] border-slate-600">{row.subtipo}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
 
 export default TableView;
--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 import Image from "next/image";
 import { Avatar, IconButton } from "@mui/material";
 import { useState } from "react";
@@ -6,11 +6,13 @@ import Carrinho from "./components/Carrinho";
 import TableView from "./components/Table";
 
 export default function Home() {
-  const [hidden, setHidden] = useState(true)
+  const [hidden, setHidden] = useState(true);
   return (
-    <main className="min-h-screen">
-      <Carrinho />
-      <TableView />
+    <main className="min-h-screen flex justify-center">
+      <div className="">
+        <Carrinho classes={""} />
+        <TableView classes={"flex justify-center"} />
+      </div>
     </main>
   );
 }
